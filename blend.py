c6e06a3c54d9fa6fd5b335fa1ddc30707184d22a
Shawn Mathew <mathewshawn99@gmail.com>
2020-03-13 16:49:21 -0500

    Added GPU selection

--- blend.py
+++ blend.py
@@ -5,12 +5,24 @@ Created on Fri Mar 13 11:11:34 2020
 @author: mathesn
 """
 
-import bpy
+import bpy, _cycles
 import time
 import math
 import os
 
+for card in bpy.context.user_preferences.addons['cycles'].preferences.devices:
+    print(card.name)
+
+prefs = bpy.context.user_preferences.addons['cycles'].preferences
+prefs.compute_device_type = 'CUDA'
+prefs.devices[0].use = True
+
+bpy.ops.wm.save_userpref()
+
+
+
 
 print("Running")
 
-bpy.ops.render.render( animation=True )
+bpy.data.scenes['Scene'].render.filepath = os.path.join("test.png")
+bpy.ops.render.render()
