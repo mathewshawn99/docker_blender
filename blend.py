ac0cfe8aa9417b25adb14e0797959dabda45a35e
Shawn Mathew <mathew_shawn_n@cat.com>
2020-03-13 14:27:12 -0500

    Added test script to control blender functionality

---
+++ blend.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 11:11:34 2020
+
+@author: mathesn
+"""
+
+import bpy
+import time
+import math
+import os
+
+
+print("Running")
+
+bpy.ops.render.render( animation=True )
