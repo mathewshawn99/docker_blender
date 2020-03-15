b92d8921f150fc61c16cac4ed1e7972427f7bdb4
Shawn Mathew <mathew_shawn_n@cat.com>
2020-03-15 15:08:59 -0500

    fixed logic to save output file

--- blend.py
+++ blend.py
@@ -12,13 +12,6 @@ import os
 
 import datetime
 
-x = datetime.datetime.now()
-LOG_FILE_NAME = x.strftime("%Y-%m-%d %H-%M-%S") + ".txt"
-LOG_FILE= open("logs/" + LOG_FILE_NAME,"w+")
-
-#start time
-LOG_FILE.write("Began at:" + str(datetime.datetime.now()))
-
 for card in bpy.context.user_preferences.addons['cycles'].preferences.devices:
     print(card.name)
 
@@ -28,11 +21,21 @@ prefs.devices[0].use = True
 
 bpy.ops.wm.save_userpref()
 
+
+x = datetime.datetime.now()
+LOG_FILE_NAME = x.strftime("%Y-%m-%d %H-%M-%S") + ".txt"
+LOG_FILE= open("logs/" + LOG_FILE_NAME,"w+")
+
+#start time
+LOG_FILE.write("Began at:" + str(datetime.datetime.now()))
+
+
+
 print("Running")
 
-bpy.data.scenes['Scene'].render.filepath = os.path.join("test.png")
-bpy.ops.render.render()
+bpy.context.scene.render.filepath = "test.png"
+bpy.ops.render.render(write_still = True)
 
-LOG_FILE.write("\Done rendering at:" + str(datetime.datetime.now()))
+LOG_FILE.write("\nDone rendering at:" + str(datetime.datetime.now()))
   
 LOG_FILE.close()
